6633d250bc9218dd0279fa4f2edc8434d7467c4a
DaveD123321 <davedai@d-i89-148-181.student.eduroam.uq.edu.au>
2022-08-17 09:23:40 +1000

    update the voice level testing function

--- script.js
+++ script.js
@@ -6,3 +6,44 @@ $(document).ready(function(c) {
         });
     });	  
 });
+
+function notSupport() {
+    mystatus.innerHTML = "Not support this browser"
+}
+
+function onError() {
+    mystatus.innerHTML = "Something wrong"
+}
+
+function onSuccess(stream) {
+
+    audioContext = window.AudioContext || window.webkitAudioContext;
+    context = new audioContext();
+    audioInput = context.createMediaStreamSource(stream);
+
+    volume = context.createGain();
+    audioInput.connect(volume);
+    var bufferSize = 2048;
+
+    recorder = context.createScriptProcessor(bufferSize, 1, 1);
+
+    recorder.onaudioprocess = function (e) {
+        var buffer = e.inputBuffer.getChannelData(0);
+        var maxVal = 0;
+
+        for (var i = 0; i < buffer.length; i++) {
+            if (maxVal < buffer[i]) {
+                maxVal = buffer[i];
+            }
+        }
+
+        mystatus.innerHTML = "Voice Level: " + Math.round(maxVal * 10)*10;
+
+        if (maxVal > .7) {
+            mystatus.innerHTML = "Too loud!!";
+        }
+    }
+
+    volume.connect(recorder);
+    recorder.connect(context.destination);
+}
